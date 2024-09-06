73b69e90ecc8006caaee9e19902c033ba4e367cf
TooManyLs <dimonzin@icloud.com>
2024-09-07 06:16:06 +0700

    Add Comment.nvim, indent-blankline.nvim, gitsigns, treesitter, mason, telescope and their configurations

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,30 +1,168 @@
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require('config.settings')
 require('config.lazy')
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconf = require('lspconfig')
-
---lspconf.ruff.setup({
---    capabilities = capabilities,
---    init_options = {
---        settings = {
---            configuration = '~/.config/ruff/ruff.toml',
---            lineLength = 79,
---            showSyntaxErrors = true,
---        },
---    },
---})
-
-lspconf.pyright.setup{
-    capabilities = capabilities,
-} 
-
-lspconf.rust_analyzer.setup {
-    capabilities = capabilities,
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+  on_attach = function(bufnr)
+    vim.keymap.set('n', '[c', require('gitsigns').prev_hunk, { buffer = bufnr })
+    vim.keymap.set('n', ']c', require('gitsigns').next_hunk, { buffer = bufnr })
+  end,
 }
 
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Add indent guides
+require("ibl").setup({
+	indent = { char = "┊" },
+	whitespace = { remove_blankline_trail = false },
+})
+
+require('Comment').setup()
+
+-- Telescope
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+-- Enable telescope fzf native
+require('telescope').load_extension 'fzf'
+
+-- Add leader shortcuts
+vim.keymap.set('n', '<leader><space>', function() require('telescope.builtin').buffers { sort_lastused = true } end)
+vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files { previewer = false } end)
+vim.keymap.set('n', '<leader>sb', function() require('telescope.builtin').current_buffer_fuzzy_find() end)
+vim.keymap.set('n', '<leader>sh', function() require('telescope.builtin').help_tags() end)
+vim.keymap.set('n', '<leader>st', function() require('telescope.builtin').tags() end)
+vim.keymap.set('n', '<leader>sd', function() require('telescope.builtin').grep_string() end)
+vim.keymap.set('n', '<leader>sp', function() require('telescope.builtin').live_grep() end)
+vim.keymap.set('n', '<leader>?', function() require('telescope.builtin').oldfiles() end)
+
+-- Treesitter configuration
+-- Parsers must be installed manually via :TSInstall
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<M-space>',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
+}
+
+-- LSP settings
+require('mason').setup {}
+require('mason-lspconfig').setup()
+
+local lspconf = require('lspconfig')
+local on_attach = function(_, bufnr)
+  local attach_opts = { silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, attach_opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, attach_opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, attach_opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, attach_opts)
+  vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, attach_opts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, attach_opts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, attach_opts)
+  vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, attach_opts)
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, attach_opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, attach_opts)
+  vim.keymap.set('n', 'so', require('telescope.builtin').lsp_references, attach_opts)
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities =require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+local servers = { 'pyright', 'rust_analyzer' }
+for _, lsp in ipairs(servers) do
+    lspconf[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
+end
+
+-- Autocompletion setup
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
+
+luasnip.config.setup {}
 
 cmp.setup ({
     enabled = function()
--- nvim/.config/nvim/lua/plugins/spec.lua
+++ nvim/.config/nvim/lua/plugins/spec.lua
@@ -1,10 +1,34 @@
 return {
-    { 'hrsh7th/nvim-cmp' },
+    -- LSP
     { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    -- autocompletion
+    { 'hrsh7th/nvim-cmp' },
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/cmp-nvim-lsp-signature-help' },
     { 'hrsh7th/cmp-buffer' },
     { 'hrsh7th/cmp-path' },
     { 'saadparwaiz1/cmp_luasnip' }, 
     { 'L3MON4D3/LuaSnip' },
+
+    -- git highlighting
+    { 'lewis6991/gitsigns.nvim' },
+    -- line/block commenting
+    { 'numToStr/Comment.nvim' },
+    -- indentation guides
+    { 'lukas-reineke/indent-blankline.nvim' },
+
+    -- syntax highlighting
+    { 'nvim-treesitter/nvim-treesitter' },
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
+
+    -- Fuzzy Finder (files, lsp, etc)
+    { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      -- NOTE: If you have trouble with this installation, refer to the README for telescope-fzf-native.
+      build = 'make',
+    },      
 }
