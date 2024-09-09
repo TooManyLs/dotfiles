61db3fb3938106823e1714e4f24a9bbbef9f1cab
TooManyLs <dimonzin@icloud.com>
2024-09-09 10:26:46 +0700

    Add more settings for pleasant experience

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -1,20 +1,28 @@
-
+vim.opt.guicursor = ''
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
+-- vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.wo.signcolumn = 'yes'
 vim.opt.completeopt = 'menuone,noselect'
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = '80'
 
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
