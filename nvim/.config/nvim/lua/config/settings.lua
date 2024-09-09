95e1c3efd247d961aece96e7d494d76531092fc8
TooManyLs <dimonzin@icloud.com>
2024-09-10 01:46:56 +0700

    Added undotree plugin

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -26,3 +26,12 @@ vim.opt.colorcolumn = '80'
 
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
