134ba922a41320ff90098e6605a699ea1a1aad7f
TooManyLs <dimonzin@icloud.com>
2024-09-09 10:27:54 +0700

    Change theme selection method, auto transparency, add new theme

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -3,16 +3,10 @@ return {
         "hachy/eva01.vim",
         lazy = false,
         priority = 1000,
-        config = function()
-          vim.cmd.colorscheme "eva01"
-        end,
     },
     {
-      "Tsuzat/NeoSolarized.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
---        config = function()
---          vim.cmd [[ colorscheme NeoSolarized ]]
---        end
+        "polirritmico/monokai-nightasty.nvim",
+          lazy = false,
+          priority = 1000,
     },
 }
