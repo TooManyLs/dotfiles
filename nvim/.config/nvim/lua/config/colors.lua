27bed4277ea77dbf3d5b1b303dcdd20990f9646b
TooManyLs <dimonzin@icloud.com>
2024-09-23 15:17:25 +0700

    Make telescope bg transparent

--- nvim/.config/nvim/lua/config/colors.lua
+++ nvim/.config/nvim/lua/config/colors.lua
@@ -3,8 +3,14 @@ function ColorsApply(color)
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#22d0d0", bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LazyNormal", { bg = "#262626" })
+    vim.api.nvim_set_hl(0, "MasonNormal", { bg = "#262626" })
 end
 
 ColorsApply()
