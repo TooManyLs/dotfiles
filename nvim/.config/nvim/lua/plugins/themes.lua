be6ac9f0a4430af31f329046ea8fe37a40a9f5fe
TooManyLs <dimonzin@icloud.com>
2024-10-15 21:16:35 +0700

    Small cosmetic changes

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,10 +1,5 @@
 return {
     {
-        "hachy/eva01.vim",
-        lazy = false,
-        priority = 1000,
-    },
-    {
         "polirritmico/monokai-nightasty.nvim",
           lazy = false,
           priority = 1000,
