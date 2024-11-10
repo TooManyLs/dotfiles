d859d08ad4718b20ea7dd2b4538f6812c0ed6955
TooManyLs <dimonzin@icloud.com>
2024-11-10 13:21:17 +0700

    update diagnostics in insert mode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -258,3 +258,9 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
 vim.diagnostic.config{
   float={border=_border}
 }
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    update_in_insert = true
+  }
+)
