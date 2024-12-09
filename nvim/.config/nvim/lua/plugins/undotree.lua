c41422c122a7e3223e9b51c913c20ef0b98288fe
TooManyLs <dimonzin@icloud.com>
2024-12-09 09:52:18 +0700

    Modify undotree config

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,4 +1,9 @@
 return {
     "mbbill/undotree",
-    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle),
+    config = function()
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_ShortIndicators = 1
+    end,
 }
